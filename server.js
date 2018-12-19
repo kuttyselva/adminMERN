bc1505f8287f3558e525fa069421dc30cf946e8b
kuttyselva <kuttyselva57@gmail.com>
2018-12-20 01:16:18 +0530

    express and mongo-git kraken update

--- server.js
+++ server.js
@@ -15,5 +15,6 @@ app.get('/',(req,res) => res.send('Hello kutty'));
 app.use('/api/users',users);
 app.use('/api/profile',profile);
 app.use('/api/posts',posts);
+
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
