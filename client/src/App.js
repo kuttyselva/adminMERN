3765dcba60064d55ef3e14c83fa79861e2b6552e
unknown <kuttyselva57@gmail.com>
2019-01-07 01:10:08 +0530

    Redux day2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import './App.css';
@@ -10,6 +12,7 @@ import Login from './components/auth/Login';
 class App extends Component {
   render() {
     return (
+      <Provider store={ store }>
       <Router>
       <div className="App">
         <Navbar/>
@@ -19,6 +22,7 @@ class App extends Component {
         <Footer/>
       </div>
       </Router>
+      </Provider>
     );
   }
 }
