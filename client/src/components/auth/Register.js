2b9dd340b1b617bd75c2ecab1f03f0547dbfb67c
kuttyselva <kuttyselva57@gmail.com>
2019-01-09 01:28:40 +0530

    signup day2
    REdux signup | axios

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import PropTypes from 'prop-types';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {registeruser} from '../../actions/authActions'
+
 import classnames from 'classnames';
-export default class Register extends Component{
+ class Register extends Component{
 constructor(){
     super();
     this.state={
@@ -17,6 +21,11 @@ constructor(){
 onChange(e){
     this.setState({[e.target.name]: e.target.value});
 }
+componentWillReceiveProps(nextProps){
+if(nextProps.errors){
+  this.setState({errors:nextProps.errors});
+}
+}
 onSubmit(e){
     e.preventDefault();
     const newUser={
@@ -25,14 +34,15 @@ onSubmit(e){
         password:this.state.password,
         password2:this.state.password2
     };
-    axios.post('/api/users/register',newUser)
-    .then(res=>console.log(res.data))
-    .catch(err => this.setState({errors:err.response.data}));
+    this.props.registeruser(newUser,this.props.history);
+   
 }
     render(){
+     
         const {errors} =this.state;
         return(
             <div className="register">
+           
             <div className="container">
               <div className="row">
                 <div className="col-md-8 m-auto">
@@ -65,3 +75,13 @@ onSubmit(e){
         );
     }
 }
+Register.propTypes={
+  registeruser: PropTypes.func.isRequired,
+  auth:PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps=(state)=>({
+  auth:state.auth,
+  errors:state.errors
+});
+export default connect(mapStateToProps,{registeruser})(withRouter(Register));
